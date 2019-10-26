eda53cdc730a381c0d997460e47b170a27bd81d8
Lily Chon <LChon@Perkinscoie.com>
2019-10-26 13:38:35 -0700

    Fixed html + css and added divs for js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,20 @@ $(document).ready(function () {
 
     $('body').css('display', 'none').fadeIn(1000);
 })
+
 $(document).on("click", "#goBtn", displayMapContent);
 
 function displayMapContent() {
-    $("body").empty();
+    $(".grid-container").empty();
+
+    $("<div>", {
+        html: "hello",
+        appendTo: ".map"
+    })
+
+    $("<iframe>", {
+        src: "https://maps.google.com/maps?q=seattle&t=&z=13&ie=UTF8&iwloc=&output=embed",
+        class: "framedMap",
+        appendTo: ".map"
+    })
 }
