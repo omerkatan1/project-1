83ea23061e16a95de72020ca62ac42619d147ebd
Lily Chon <LChon@Perkinscoie.com>
2019-10-31 20:04:50 -0700

    Got rid of background image

--- Assets/displayWeather.js
+++ Assets/displayWeather.js
@@ -48,10 +48,7 @@ function displayWeatherInfo(weather) {
 
     if (weather.icon === "Clear") {
         $("#icon").addClass("fas fa-sun");
-        $('body').css({
-            backgroundImage : 'url("Assets/Images/sunny.jpg")', 'background-repeat': 'no-repeat',
-            class: "backgroundImg"
-        });    }
+    }
     else if (weather.icon === "Clouds") {
         $("#icon").addClass("fas fa-cloud");
     }
