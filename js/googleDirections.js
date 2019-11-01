de0867bff6be6557f03607d7d6e4d5ceaa13f912
Pedja <vpedja12@gmail.com>
2019-10-31 21:37:27 -0700

    Fixed cors function

--- js/googleDirections.js
+++ js/googleDirections.js
@@ -1,11 +1,11 @@
-function googleDirections(startLat, startLong, addressLat, addressLon) {
-    var googleKey = "&key=AIzaSyB-VF7nGUXZizMU85BJnAQ06Qg_acuZYJw";
-    var googleDirectionsURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + startLat + "," + startLong + "&destination=" + addressLat + "," + addressLon + googleKey;
-    $.ajax({
-      url: googleDirectionsURL,
-      method: "GET"
-    }).then(function (res) {
-      console.log("Directions ran");
-      console.log(res);
-  })
-}
+// function googleDirections(startLat, startLong, addressLat, addressLon) {
+//     var googleKey = "&key=AIzaSyB-VF7nGUXZizMU85BJnAQ06Qg_acuZYJw";
+//     var googleDirectionsURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + startLat + "," + startLong + "&destination=" + addressLat + "," + addressLon + googleKey;
+//     $.ajax({
+//       url: googleDirectionsURL,
+//       method: "GET"
+//     }).then(function (res) {
+//       console.log("Directions ran");
+//       console.log(res);
+//   })
+// }
