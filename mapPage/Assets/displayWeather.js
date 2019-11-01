d4f6f5ad7c82cbed27eb9f6d6002f809b7b92327
ogata216 <amidstasinglebreath@gmail.com>
2019-10-31 19:21:18 -0700

    day/night background update

--- mapPage/Assets/displayWeather.js
+++ mapPage/Assets/displayWeather.js
@@ -16,9 +16,11 @@ function getWeatherInfo(city) {
             tempHigh: response.main.temp_max.toFixed(1),
             tempLow: response.main.temp_min.toFixed(1),
             wind: response.wind.speed.toFixed(1),
-            icon: response.weather[0].main
+            icon: response.weather[0].main,
+            lat: response.coord.lat,
+            lon: response.coord.lon,
         }
- 
+
         displayWeatherInfo(weather);
     })
 }
@@ -45,6 +47,24 @@ function displayWeatherInfo(weather) {
         appendTo: ".weatherInfo"
     })
 
+    //start point of time based background
+
+    //may need to be altered if the trip crosses time zones
+    var curhour = (new Date()).getHours();
+    // returns information based on 24 hour clock
+    //getSRSS(weather.lat, weather.lon);
+    var sunrise = 7;
+    var daylength = 10;
+    var sunset = sunrise + daylength;
+    if(curhour >= sunrise && curhour<= sunset){
+        $(".weatherInfo").removeClass("nightbody").addClass("daybody");
+    } else {
+        $(".weatherInfo").removeClass("daybody").addClass("nightbody");
+    }
+
+    //end point of time based background
+
+
 
     if (weather.icon === "Clear") {
         $("#icon").addClass("fas fa-sun");
@@ -61,4 +81,33 @@ function displayWeatherInfo(weather) {
     else if (weather.icon === "Rain") {
         $("#icon").addClass("fas fa-cloud-showers-heavy");
     }
+}
+
+function getSRSS(lat, lng) {
+    var queryURL = "https://api.sunrise-sunset.org/json?lat=" + lat + "&lng=" + lng;
+    console.log("latitude:" + lat);
+    console.log("longitude:" + lng);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        console.log("Sunrise:" + response.results.sunrise);
+        console.log("Sunset:" + response.results.sunset);
+
+        // api seems to return inaccurate data, future versions will use a different api or program for correction along the following design.
+        // also add minute determination for greater accuraccy.
+        /*
+        var curhour = (new Date()).getHours();
+        var sunrise = response.results.sunrise[0];
+        var sunset = sunrise + response.results.day_length[1];
+        if(curhour >= sunrise && curhour<= sunset){
+            $(".weatherInfo").removeClass("nightbody").addClass("daybody");
+        } else {
+            $(".weatherInfo").removeClass("daybody").addClass("nightbody");
+        }
+         */
+
+    })
 }
